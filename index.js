cfe06fcc719f4a5ee77b87182dfcc348c464df92
Parth Garg <gargparthb@gmail.com>
2021-05-06 20:28:49 -0400

    v1

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+require("dotenv").config();
+
+let techChannelID = '840000105535897610';
 
 client.once('ready', () => {
   console.log('Allahu Akbar');
+  let techChannel =
+    setInterval(() => {
+      client.channels.fetch(techChannelID).then(channel => channel.send("NO HARAM"))
+    }, 60 * 60 * 1000);
 });
 
 // TODO: .env this
-client.login('ODQwMDAyMDQ5NTQxNjY4ODc0.YJR23w.x6rwd8BthOblGqBGjiP6KQjVwUw');
+client.login(process.env.BOT_ENV);
 
-techChannelID = '840000105535897610';
 client.on('message', msg => {
   if (msg.channel.id == techChannelID && msg.content == "Allahu Akbar") {
     msg.channel.send('Peace be with you Brother!');
