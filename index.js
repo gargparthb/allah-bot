fd9af8367a65ab915d57460db7103399fecced28
Parth Garg <gargparthb@gmail.com>
2021-05-13 21:13:50 -0400

    shame command

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 require("dotenv").config();
 
-let techChannelID = '840000105535897610';
-
 client.once('ready', () => {
   console.log('Allahu Akbar');
 });
@@ -13,5 +11,14 @@ client.login(process.env.BOT_TOKEN);
 client.on('message', msg => {
   if (msg.content == "Allahu Akbar") {
     msg.channel.send('Peace be with you Brother!');
+  }
+
+  if (msg.content.startsWith('!') && !msg.author.bot) {
+    const args = msg.content.slice(1).trim().split(/ +/);
+    const command = args.shift().toLowerCase();
+
+    if (command === 'shame') {
+      msg.channel.send(`${msg.author.toString()} has shamed ${msg.mentions.members.find(() => true).toString()}`);
+    }
   }
 });
