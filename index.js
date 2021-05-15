fdb20c5ca2f6b70776a9a767146f87ede34e8de4
gargparthb <62770333+gargparthb@users.noreply.github.com>
2021-05-15 14:13:09 -0400

    reasons

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const fs = require('fs');
-const { Client, Collection } = require('discord.js');
+const {
+  Client,
+  Collection
+} = require('discord.js');
 const client = new Client();
 require("dotenv").config();
 
