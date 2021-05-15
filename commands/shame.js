5e67e6c66630463619d7f56d1019a6cc0569e15e
gargparthb <62770333+gargparthb@users.noreply.github.com>
2021-05-15 01:25:59 -0400

    embeds

--- commands/shame.js
+++ commands/shame.js
@@ -1,21 +1,36 @@
-const { MessageEmbed } = require("discord.js");
+const {
+  MessageEmbed
+} = require("discord.js");
 
 module.exports = {
   name: 'shame',
   execute(msg, args, client) {
+    // gets the guild member mentions
     const shamee = msg.mentions.members.first();
+    // seperates the caliph shamming case
     if (shamee.roles.cache.some(role => role.name == 'Caliph')) {
-      const caliphEmbed = new MessageEmbed();
-      msg.channel.send(`Shame on ${message.author.toString()}, for trying to shame the Caliph!`);
-    } else {
-      const embed = new MessageEmbed()
+      const caliphEmbed = new MessageEmbed()
         .setTitle(`A Public Shaming,`)
-        .setColor([0, 135, 0])
+        .setColor('RANDOM')
+        .setThumbnail(client.user.displayAvatarURL())
+        .addField('Shamer:', client.user.toString(), true)
+        .addField('Shamee:', msg.author.toString(), true)
+        .addField('Reason:', "For attempting to shame to Caliph!");
+      msg.channel.send(caliphEmbed);
+    } else {
+      let shameEmbed = new MessageEmbed()
+        .setTitle(`A Public Shaming,`)
+        .setColor('RANDOM')
         .setThumbnail(client.user.displayAvatarURL())
         .addField('Shamer:', msg.author.toString(), true)
         .addField('Shamee:', shamee.toString(), true);
 
-      msg.channel.send(embed);
+      // maybe add a reason
+      if (args.length > 1) {
+        shameEmbed.addField('Reason:', args.slice(1).join(' '), false);
+      }
+
+      msg.channel.send(shameEmbed);
     }
   }
 }
